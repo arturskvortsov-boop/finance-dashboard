f90ea2a790597eef2aa1fd0bdce60421cc74fdec
arturskvortsov-boop <artur.skvortsov@icloud.com>
2026-09-18 23:01:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1983,6 +1983,7 @@ function switchPage(page,direction){
             if(rateHistoryChart){try{rateHistoryChart.resize();}catch(e){}}
             animateCurrencyNumbers();
             renderRateFreshness();
+            renderCbrRate();
         },80);
     } else if(page==='settings'){
         updateTokenStatus();
